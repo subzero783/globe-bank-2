ae2bb623028d3de1fab3ffeb559a96e987102581
Gustavo Amezcua <gustavo@vsslagency.com>
2020-06-20 16:15:01 -0700

    added remote connection to Hiperstudio.org/qasrep-dev database

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,5 +1,5 @@
 <?php 
-define("DB_SERVER", 'localhost');
+define("DB_SERVER", 'hiperstudio.org');
 define("DB_USER", 'qasrep_dev_user');
 define("DB_PASS", 'qasrep');
 define("DB_NAME", 'qasrep_dev');
